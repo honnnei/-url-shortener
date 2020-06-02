71132806873b91d35a4487bed09f1881e819f3d8
honnnei <hannpop@gmail.com>
2020-06-02 17:15:03 +0200

    delete request works

--- app.py
+++ app.py
@@ -36,16 +36,16 @@ def index():
 
         # all_urls=all_urls
 
-# @app.route('/delete/<int:id>')
-# def delete(id):
-#     task_to_delete = Todo.query.get_or_404(id)
+@app.route('/delete/<int:id>')
+def delete(id):
+    url_to_delete = YourUrl.query.get_or_404(id)
 
-#     try:
-#         db.session.delete(task_to_delete)
-#         db.session.commit()
-#         return redirect('/')
-#     except:
-#         return 'There was a problem deleting that task'
+    try:
+        db.session.delete(url_to_delete)
+        db.session.commit()
+        return redirect('/')
+    except:
+        return 'There was a problem deleting that task'
 
 # @app.route('/update/<int:id>', methods=['GET', 'POST'])
 # def update(id):
